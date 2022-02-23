a442f0d8de80cabbaf31af1c4778b416a9c9ae5f
Greger Sundvall <gsundvall@gmail.com>
2022-02-23 15:26:14 +0100

    contains number done

--- ContainsNumber/ContainsNumber.cpp
+++ ContainsNumber/ContainsNumber.cpp
@@ -4,12 +4,19 @@
 using namespace std;
 
 
-void containsNumber()
+
+int main(int argc, char* argv[])
 {
-    int number = 123123;
-    string numberString = std::to_string(number);
-    char query = '5';
+    int number;
+    cout<<"Give me a number:"<<endl;
+    cin>>number;
+    
+    char query;
+    cout<<"What should I search for?"<<endl;
+    cin>>query;
+    
     bool doesContain = false;
+    string numberString = std::to_string(number);
 
     for (size_t i = 0; i < size(numberString) -1; ++i)
     {
@@ -23,4 +30,5 @@ void containsNumber()
     doesContain ? cout << number << " does contain the number " << query << endl :
                     cout << number << " does not contain the number " << query << endl;
     
+    return 0;
 }
